ea7501218ea2a02d261c589265b62809f2ffbbd3
Luke <33379089+lucas-j-k@users.noreply.github.com>
2019-04-08 21:20:27 +0100

    Production build ready to deploy

---
+++ build/precache-manifest.b5dea97e1243de5864bcde22c6618cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af64368600f147e7da40",
+    "url": "/static/js/main.510d0d4d.chunk.js"
+  },
+  {
+    "revision": "3173ff331c4cdf43656e",
+    "url": "/static/js/2.afb5be2a.chunk.js"
+  },
+  {
+    "revision": "af64368600f147e7da40",
+    "url": "/static/css/main.343dab48.chunk.css"
+  },
+  {
+    "revision": "3173ff331c4cdf43656e",
+    "url": "/static/css/2.2d0f1cf0.chunk.css"
+  },
+  {
+    "revision": "826120bed32cc6e75c4a07678f440800",
+    "url": "/index.html"
+  }
+];
